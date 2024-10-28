9241040e194efb7c9af16667e7f204c97cbb0c00
kingtsuna <113575193+kingtsuna@users.noreply.github.com>
2024-10-28 10:38:03 +0530

    Update oil_price_prediction.py

--- oil_price_prediction.py
+++ oil_price_prediction.py
@@ -5,11 +5,22 @@ import re
 import PyPDF2  # For PDF file reading
 import pandas as pd
 from newspaper import Article  # For website content extraction
-import nltk
-nltk.download('punkt')
+# import nltk
+# nltk.download('punkt')
 import google.generativeai as genai
 import os
 from io import BytesIO
+
+
+import nltk
+import streamlit as st
+
+# Ensure punkt data is downloaded
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
 
 # Download the punkt tokenizer models
 nltk.download('punkt')
